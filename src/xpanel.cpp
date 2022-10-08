e0cb32640b2506de4f50f2c78b963b3d9e4c941c
László Várady <laszlo.varady@protonmail.com>
2022-10-08 15:58:06 +0200

    xpanel: remove unused OpenGL include
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>

--- src/xpanel.cpp
+++ src/xpanel.cpp
@@ -47,13 +47,6 @@ BOOL APIENTRY DllMain(HANDLE hModule,
 	return true;
 }
 #endif
-#if LIN
-#include <GL/gl.h>
-#elif __GNUC__
-#include <OpenGL/gl.h>
-#else
-#include <GL/gl.h>
-#endif
 
 #ifndef XPLM300
 #error This is made to be compiled against the XPLM300 SDK
